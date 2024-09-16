58a57b58ccd65ac9cf86ef04f5f3d773b4f05b28
Shrijana Karki <srznakrki111@gmail.com>
2024-09-16 16:21:38 +0545

    Removed Unused components from main.js

--- ems_frontend/src/main.js
+++ ems_frontend/src/main.js
@@ -9,12 +9,7 @@ import Select from 'primevue/select'
 import InputNumber from 'primevue/inputnumber'
 import Column from 'primevue/column'
 import DataTable from 'primevue/datatable'
-
 import Dialog from 'primevue/dialog';
-import Dropdown from 'primevue/dropdown';
-import Toolbar from 'primevue/toolbar';
-import ToastService from 'primevue/toastservice';
-import Toast from 'primevue/toast';
 
 import 'primeicons/primeicons.css';
 
@@ -23,19 +18,13 @@ const app = createApp(App)
 app.use(router)
 app.use(PrimeVue);
 
-app.use(ToastService);
-
 app.component('Button', Button);
 app.component('InputText', InputText);
 app.component('Select', Select);
 app.component('InputNumber', InputNumber);
 app.component('Column', Column);
 app.component('DataTable', DataTable);
-
-app.component('Toolbar', Toolbar);
-app.component('Toast', Toast);
 app.component('Dialog', Dialog);
-app.component('Dropdown', Dropdown);
 
 
 app.mount('#app')
